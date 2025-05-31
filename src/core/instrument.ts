06aff605fd1c1220d20540a1ba3f93b7552b2841
Ashik Nesin <mail@ashiknesin.com>
2025-05-31 22:39:36 +0530

    Added sentry dsn

--- src/core/instrument.ts
+++ src/core/instrument.ts
@@ -4,7 +4,7 @@ import { env } from "@/env";
 // Initialize Sentry as early as possible
 Sentry.init({
   // Use DSN from environment variables or fall back to the provided one if not set
-  dsn: env.SENTRY_DSN || "https://d618b779ecf8d39e13ef32c2965882f6@o223853.ingest.us.sentry.io/4509418816733184",
+  dsn: env.SENTRY_DSN,
   
   // Setting this option to true will send default PII data to Sentry
   // For example, automatic IP address collection on events
