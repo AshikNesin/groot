be445fb677b320740628c453ee8368c79f31a4ca
Ashik Nesin <mail@ashiknesin.com>
2026-02-01 21:25:14 +0530

    Remove Basic Auth and fix auth issue

--- server/src/test/setup.ts
+++ server/src/test/setup.ts
@@ -1,4 +1,2 @@
 process.env.DATABASE_URL ??= "postgresql://user:password@localhost:5432/test";
-process.env.BASIC_AUTH_USERNAME ??= "admin";
-process.env.BASIC_AUTH_PASSWORD ??= "password";
 process.env.NODE_ENV ??= "test";
